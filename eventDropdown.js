037cb190213d3d9fc0d7a964abef4f5aed4ef0e6
GemboJones <gembojones@gmail.com>
2024-12-11 01:06:47 +0000

    adds functionality to close dropdown list when user clicks outside element

--- eventDropdown.js
+++ eventDropdown.js
@@ -33,6 +33,22 @@ selectBtn.addEventListener("click", () => {
   selectBtn.classList.toggle("open");
 });
 
+const dropdown = document.querySelector(".dropdown");
+const arrowButton = document.querySelector(".arrow-dwn");
+const arrow = document.querySelector("i");
+btnText = document.querySelector(".btn-text");
+document.addEventListener("click", (e) => {
+  if (
+    !dropdown.contains(e.target) &&
+    e.target !== selectBtn &&
+    e.target !== arrowButton &&
+    e.target !== arrow &&
+    e.target !== btnText
+  ) {
+    selectBtn.classList.remove("open");
+  }
+});
+
 let formSubmit = document.querySelector("form");
 formSubmit.addEventListener("submit", (e) => {
   e.preventDefault();
