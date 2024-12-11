d0bb3a10201b8c4f023889be9c6830b13d01f1a6
GemboJones <gembojones@gmail.com>
2024-12-11 02:03:26 +0000

    fix error attempt4

--- eventDropdown.js
+++ eventDropdown.js
@@ -34,20 +34,20 @@ selectBtn.addEventListener("click", () => {
 });
 
 // const dropdown = document.querySelector(".dropdown");
-// const arrowButton = document.querySelector(".arrow-dwn");
-// const arrow = document.querySelector("i");
-// btnText = document.querySelector(".btn-text");
-// document.addEventListener("click", (e) => {
-//   if (selectBtn.classList.contains("open") &&
-//     !dropdown.contains(e.target) &&
-//     e.target !== selectBtn &&
-//     e.target !== arrowButton &&
-//     e.target !== arrow &&
-//     e.target !== btnText
-//   ) {
-//     selectBtn.classList.remove("open");
-//   }
-// });
+const arrowButton = document.querySelector(".arrow-dwn");
+const arrow = document.querySelector("i");
+btnText = document.querySelector(".btn-text");
+document.addEventListener("click", (e) => {
+  if (selectBtn.classList.contains("open") &&
+    !document.querySelector(".dropdown").contains(e.target) &&
+    e.target !== selectBtn &&
+    e.target !== arrowButton &&
+    e.target !== arrow &&
+    e.target !== btnText
+  ) {
+    selectBtn.classList.remove("open");
+  }
+});
 
 let formSubmit = document.querySelector("form");
 formSubmit.addEventListener("submit", (e) => {
